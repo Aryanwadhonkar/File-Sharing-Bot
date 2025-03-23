0277b2f43ac63302987c9cd5b7ed43af7f47925d
Aryanwadhonkar <161907825+Aryanwadhonkar@users.noreply.github.com>
2025-03-23 21:09:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ from logging.handlers import RotatingFileHandler
 
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8060637136:AAEPWVz-Oh0qeLwaS5_JwcNsygGh3CsCaZQ")
+API_ID = int(os.environ.get("API_ID", "24103884"))
+API_HASH = os.environ.get("API_HASH", "257516f22417652d03896cb2e007d6df")
 
 
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
-DB_URL = os.environ.get("DB_URL", "")
+OWNER_ID = int(os.environ.get("OWNER_ID", "1672634667"))
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://Wleakshere:Thunderstrikes27@wleakshere.api7w.mongodb.net/?retryWrites=true&w=majority&appName=Wleakshere")
 DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
 
 
@@ -19,7 +19,7 @@ CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 
 
-FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "600")) # auto delete in seconds
+FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "1000")) # auto delete in seconds
 
 
 PORT = os.environ.get("PORT", "8080")
@@ -28,8 +28,8 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 
 try:
-    ADMINS=[6848088376]
-    for x in (os.environ.get("ADMINS", "6848088376").split()):
+    ADMINS=[1672634667]
+    for x in (os.environ.get("ADMINS", "1672634667").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
@@ -56,10 +56,9 @@ BOT_STATS_TEXT = "<b>BOT UPTIME :</b>\n{uptime}"
 
 
 
-USER_REPLY_TEXT = "❌Don't Send Me Messages Directly I'm Only File Share Bot !"
+USER_REPLY_TEXT = "Baka! I'm just here to make you smile, not to be your property!"
 
-START_MSG = os.environ.get("START_MESSAGE", "Hello {mention}\n\nI Can Store Private Files In Specified Channel And Other Users Can Access It From Special Link.")
-
+START_MSG = os.environ.get("START_MESSAGE", " {mention}\n\nI  Well, hello there! Your presence just turned up the heat in here. Ready for some fun?</b>")
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {mention}\n\n<b>You Need To Join In My Channel/Group To Use Me\n\nKindly Please Join Channel</b>")
 
 
@@ -67,9 +66,9 @@ FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {mention}\n\n<b>You Need 
 
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(6848088376)
+ADMINS.append(1672634667)
 
-LOG_FILE_NAME = "filesharingbot.txt"
+LOG_FILE_NAME = "wwleakshere_bot.txt"
 
 logging.basicConfig(
     level=logging.INFO,
